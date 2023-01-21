c369cb7a7fffa432bfd007d90ce4b3511997b619
Leo Lee <41759796+smjleo@users.noreply.github.com>
2023-01-21 19:55:43 +0000

    feat: add login/logout (#2)
    * feat: add login/logout
    
    * Format code with prettier
    
    * style: fix some deepsource comments
    
    * Format code with prettier
    
    * chore: convert function declaration style'
    
    * Format code with prettier
    
    * chore: change function declaration
    
    * Format code with prettier
    
    * fix: merge conflict
    
    * Format code with prettier
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

---
+++ authConfig.js
@@ -0,0 +1,35 @@
+// https://learn.microsoft.com/en-us/azure/active-directory/develop/tutorial-v2-react
+
+export const b2cPolicies = {
+    names: {
+        signUpSignIn: 'B2C_1_signup_signin',
+        editProfile: 'B2C_1_edit_profile',
+    },
+    authorities: {
+        signUpSignIn: {
+            authority: 'https://photong.b2clogin.com/photong.onmicrosoft.com/B2C_1_signup_signin',
+        },
+        editProfile: {
+            authority: 'https://photong.b2clogin.com/photong.onmicrosoft.com/B2C_1_edit_profile',
+        },
+    },
+    authorityDomain: 'photong.b2clogin.com',
+};
+
+export const msalConfig = {
+    auth: {
+        clientId: '5c64cf4f-5a01-4917-a2c5-e9a13de15f65',
+        authority: b2cPolicies.authorities.signUpSignIn.authority,
+        knownAuthorities: [b2cPolicies.authorityDomain],
+        redirectUri: 'http://localhost:3000',
+    },
+    cache: {
+        cacheLocation: 'sessionStorage', // This configures where your cache will be stored
+        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+};
+
+// Add scopes here for ID token to be used at Microsoft identity platform endpoints.
+export const loginRequest = {
+    scopes: [],
+};
---
+++ components/LoginButton.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { Button } from 'evergreen-ui';
+import { useMsal, useIsAuthenticated } from '@azure/msal-react';
+import { loginRequest } from '../authConfig';
+
+export default function LoginButton() {
+    const { instance } = useMsal();
+    const isAuthenticated = useIsAuthenticated();
+
+    const handleLogin = React.useCallback(() => {
+        instance.loginRedirect(loginRequest).catch((e) => {
+            // skipcq: JS-0002
+            console.error(e);
+        });
+    });
+
+    const handleLogout = React.useCallback(() => {
+        instance.logoutRedirect().catch((e) => {
+            // skipcq: JS-0002
+            console.error(e);
+        });
+    }, []);
+
+    return isAuthenticated ? (
+        <Button appearance="minimal" onClick={handleLogout}>
+            Log out
+        </Button>
+    ) : (
+        <Button appearance="primary" onClick={handleLogin}>
+            Log in
+        </Button>
+    );
+}
