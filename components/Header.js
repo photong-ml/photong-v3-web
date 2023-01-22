6193d9e82db49e47655c69f58284266833d6e5eb
Leo Lee <41759796+smjleo@users.noreply.github.com>
2023-01-22 15:55:21 +0000

    feat: tidy site (#6)
    * chore: edit meta description
    
    * docs: update readme
    
    * Format code with prettier
    
    * fix: header squashed in mobile
    
    * Format code with prettier
    
    * chore: remove unused imports
    
    * Format code with prettier
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- components/Header.js
+++ components/Header.js
@@ -3,20 +3,20 @@
 import React from 'react';
 import LoginButton from './LoginButton';
 
-import { Tab, Tablist, Pane, Heading, majorScale } from 'evergreen-ui';
+import { Tab, Tablist, Pane /*Heading, majorScale*/ } from 'evergreen-ui';
 import { useRouter } from 'next/router';
 
 export default function Header() {
     const router = useRouter();
     const tabs = [
-        { label: 'Home', path: '/', onSelect: () => router.push('/') },
-        { label: 'About', path: '/about', onSelect: () => router.push('/about') },
+        // { label: 'Home', path: '/', onSelect: () => router.push('/') },
+        // { label: 'About', path: '/about', onSelect: () => router.push('/about') },
     ];
 
     return (
         <Pane is="nav" position="sticky" top={0} display="flex" padding={16} background="tint1" zIndex={2}>
             <Pane flex={1} alignItems="center" display="flex">
-                <Heading marginRight={majorScale(3)}>Photong </Heading>
+                {/*<Heading marginRight={majorScale(3)}>Photong </Heading>*/}
                 <Pane display="flex" width="100%" alignItems="center" justifyContent="space-between">
                     <Tablist>
                         {tabs.map(({ label, path, onSelect }, i) => (
