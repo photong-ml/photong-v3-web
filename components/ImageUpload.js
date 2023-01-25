fb672eb72d0e4fff0722fc3dd129361698268e91
Restyled.io <commits@restyled.io>
2023-01-25 05:43:19 +0000

    Restyled by clang-format

--- components/ImageUpload.js
+++ components/ImageUpload.js
@@ -1,26 +1,26 @@
+import {Button, FileCard, FileUploader, MusicIcon, Pane} from 'evergreen-ui';
 import React from 'react';
-import { Pane, FileUploader, FileCard, Button, MusicIcon } from 'evergreen-ui';
 
-export default function ImageUpload({ marginTop, files, setFiles, generateAudio }) {
-    const [fileRejections, setFileRejections] = React.useState([]);
-    const handleChange = React.useCallback((files) => setFiles([files[0]]), []);
-    const handleRejected = React.useCallback((fileRejections) => setFileRejections([fileRejections[0]]), []);
-    const handleRemove = React.useCallback(() => {
-        setFiles([]);
-        setFileRejections([]);
-    }, []);
+export default function ImageUpload(
+    {marginTop, files, setFiles, generateAudio}) {
+  const [fileRejections, setFileRejections] = React.useState([]);
+  const handleChange = React.useCallback((files) => setFiles([ files[0] ]), []);
+  const handleRejected = React.useCallback(
+      (fileRejections) => setFileRejections([ fileRejections[0] ]), []);
+  const handleRemove = React.useCallback(() => {
+    setFiles([]);
+    setFileRejections([]);
+  }, []);
 
     return (
         <Pane width="100%" marginTop={marginTop}>
             <FileUploader
-                label="Upload Image"
-                description="The image will be used to generate a soundtrack. Image can be up to 8MB."
-                maxSizeInBytes={8 * 1024 ** 2}
-                maxFiles={1}
-                onChange={handleChange}
-                onRejected={handleRejected}
-                acceptedMimeTypes={['image/png', 'image/jpeg', 'image/webp']}
-                renderFile={(file) => {
+    label = "Upload Image"
+    description =
+        "The image will be used to generate a soundtrack. Image can be up to 8MB."
+    maxSizeInBytes = {8 * 1024 ** 2} maxFiles = {1} onChange =
+        {handleChange} onRejected = {handleRejected} acceptedMimeTypes =
+            {['image/png', 'image/jpeg', 'image/webp']} renderFile = {(file) => {
                     let { name, size, type } = file;
                     if (name.length > 20) name = name.slice(0, 20) + '...' + name.slice(-4);
                     const fileRejection = fileRejections.find((fileRejection) => fileRejection.file === file);
--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-        // runtime: 'experimental-edge',
-        optimizeCss: true,
-    },
+  reactStrictMode : true,
+  experimental : {
+    // runtime: 'experimental-edge',
+    optimizeCss : true,
+  },
 };
 
 module.exports = nextConfig;
